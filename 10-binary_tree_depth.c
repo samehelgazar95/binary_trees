ffd3c7701714e3b60174a59c2a5a4d5004c41900
Sameh ElGazar <selgazar95@gmail.com>
2024-01-30 21:16:52 +0200

    Task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the depth of a binary tree
+ * @tree: the tree root
+ * Return: the depth
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
+}
