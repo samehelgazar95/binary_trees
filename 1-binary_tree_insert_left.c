7787d9f82219a3b6c33b4e38a919a96b9f147e7f
Sameh ElGazar <selgazar95@gmail.com>
2024-01-30 06:36:34 +0200

    Task 1.1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,14 +11,14 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-
 	if (parent == NULL)
 		return (NULL);
+
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 
-	if (parent->left != NULL)
+	if (parent->left)
 	{
 		new_node->left = parent->left;
 		new_node->left->parent = new_node;
