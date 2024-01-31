0d77749a2b4ffded625e7b0c16a169394358fce6
Sameh ElGazar <selgazar95@gmail.com>
2024-01-31 03:29:22 +0200

    Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,6 +11,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 	size_t l;
 	size_t r;
 
+	if (tree == NULL)
+		return (0);
+
 	l = tree->left ? (binary_tree_balance(tree->left) + 1) : 0;
 	r = tree->right ? (binary_tree_balance(tree->right) + 1) : 0;
 
