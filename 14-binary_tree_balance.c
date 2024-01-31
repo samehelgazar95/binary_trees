0b84041bc9d210c2d0a3ef63d16ee26a5ce18e6e
Sameh ElGazar <selgazar95@gmail.com>
2024-01-31 03:26:02 +0200

    Task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - calc the balnce factor
+ * @tree: the root
+ * Return: the balance factor
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t l;
+	size_t r;
+
+	l = tree->left ? (binary_tree_balance(tree->left) + 1) : 0;
+	r = tree->right ? (binary_tree_balance(tree->right) + 1) : 0;
+
+	return (l - r);
+}
