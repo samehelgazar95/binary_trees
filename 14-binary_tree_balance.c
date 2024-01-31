92b74ca437c49ba2aec5e91bdcf5da41d2ff7270
Sameh ElGazar <selgazar95@gmail.com>
2024-01-31 03:42:42 +0200

    Task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,27 @@
 #include "binary_trees.h"
 
 /**
+ * subtree_height - measures the height of a binary tree
+ * the height is the number of the vertices
+ * @tree: the tree root
+ * Return: the height
+ */
+
+int subtree_height(const binary_tree_t *tree)
+{
+	int left = 0;
+	int right = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left = tree->left ? subtree_height(tree->left) + 1 : 1;
+	right = tree->right ? subtree_height(tree->right) + 1 : 1;
+
+	return (left > right ? left : right);
+}
+
+/**
  * binary_tree_balance - calc the balnce factor
  * @tree: the root
  * Return: the balance factor
@@ -14,8 +35,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	l = tree->left ? (binary_tree_balance(tree->left) + 1) : 0;
-	r = tree->right ? (binary_tree_balance(tree->right) + 1) : 0;
+	l = subtree_height(tree->left);
+	r = subtree_height(tree->right);
 
 	return (l - r);
 }
